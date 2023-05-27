4c51af2e4057c2e8527d0f765b2b74dcf470130f
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-05-28 01:29:21 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import noticeBoardRoutes from "./routes/Notice.js";
-import repairRoutes from "./routes/RepairPart.js";
-import machineRoutes from "./routes/Machine.js";
-import issuesRoutes from "./routes/Issue.js";
-import plannerRoutes from "./routes/Planner.js";
-import employeeRoutes from "./routes/Employee.js";
-import departmentRoutes from "./routes/Map.js";
+import noticeBoardRoutes from "./routes/noticeRoute.js";
+import repairRoutes from "./routes/repairPartsRoute.js";
+import machineRoutes from "./routes/machineRoute.js";
+import issuesRoutes from "./routes/issueRoute.js";
+import plannerRoutes from "./routes/plannerRoute.js";
+import employeeRoutes from "./routes/employeeRoute.js";
+import departmentRoutes from "./routes/departmentRoute.js";
 
 const app = express();
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use("/employees",employeeRoutes)
 app.use("/departments",departmentRoutes)
 app.use("/machines",machineRoutes)
 
-// Start the server
+// Start the server on Port 8800
 app.listen(8800, () => {
   console.log("Backend is started on PORT : 8800");
 });
