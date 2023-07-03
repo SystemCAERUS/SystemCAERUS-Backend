be420041779e17a32c41c74465d9ddb7e3ea0829
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-07-04 00:24:23 +0530

    inner join planner with other db tables

--- models/plannerModel.js
+++ models/plannerModel.js
@@ -1,7 +1,8 @@
 import db from "../config/database.js";
 
 export const getAllPlannedWorksFromDb = (callback) => {
-  const q = "SELECT * FROM planner";
+  const q = "SELECT planner.*, department.*, machine.* FROM planner INNER JOIN department ON planner.departmentID = department.id INNER JOIN machine ON planner.machineID = machine.machineid;";
+  
 
   db.query(q, (error, data) => {
     if (error) {
