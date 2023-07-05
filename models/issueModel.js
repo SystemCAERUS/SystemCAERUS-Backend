74ee2968c6cf2d59f0f37eb857af099e050cc23f
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-07-06 00:38:14 +0530

    Update issueModel.js

--- models/issueModel.js
+++ models/issueModel.js
@@ -1,7 +1,7 @@
 import db from "../config/database.js";
 
 export const getAllAvailableIssuesFromDb = (callback) => {
-  const q = "SELECT * FROM issues";
+  const q = "SELECT issues.*, department.*, machine.* FROM issues INNER JOIN department ON issues.departmentID = department.id INNER JOIN machine ON issues.machineID = machine.machineid;";
 
   db.query(q, (error, data) => {
     if (error) {
